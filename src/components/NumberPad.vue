a9fd6d0f0290caf27052a3a556bef998530c73ee
Yang <651033755@qq.com>
2020-10-23 21:23:38 +0800

    使用v-model代替:value和@input绑定

--- src/components/NumberPad.vue
+++ src/components/NumberPad.vue
@@ -4,60 +4,60 @@
       <div class="output-wrapper">
         
         <label class="notes">
-          <input type="text" :value="value" 
-          @change="onInput"
+          <input type="text"  
+          v-model="value"
           placeholder="内容备注"/>
         </label>
         <div class="output">{{output}}</div>
       </div>
 
       <div class="buttons">
+        <div class="number">
         <button @click="inputContent">1</button>
         <button @click="inputContent">2</button>
         <button @click="inputContent">3</button>
-        <button @click="remove">
-          <img src="../assets/img/left.png">
-        </button>
+
         <button @click="inputContent">4</button>
         <button @click="inputContent">5</button>
         <button @click="inputContent">6</button>
-        <button @click="plus">+</button>
+
         <button @click="inputContent">7</button>
         <button @click="inputContent">8</button>
         <button @click="inputContent">9</button>
-        <button @click="minus">-</button>
+        
         <button @click="clear">清零</button>
-        <button @click="inputContent" class="zero">0</button>
+        <button @click="inputContent" >0</button>
         <button @click="inputContent">.</button>
+</div>
+       <div class="tool">
+        <button class="day">day</button>
+        <button @click="remove">
+          <img src="../assets/img/left.png">
+        </button>
         <button @click="ok" class="ok">OK</button>
-
+</div>
       </div>
     </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import {Component, Prop, Watch} from 'vue-property-decorator';
+import {Component} from 'vue-property-decorator';
 
 @Component
 export default class NumberPad extends Vue{
-  @Prop(Number) outputValue!: number;
 
   output = '0';
   result = 0;
   value = '';
-  arr = ['0'];
   
-@Watch('output')
-onOutputChange(){
- this.$emit('update:outputValue',this.result)
-}
 
   inputContent(event: MouseEvent){
     const button = event.target as HTMLButtonElement
     const input = button.textContent as string
 
-    if(this.output.length === 11){return}
+    
+    if(this.output.length === 11){return}  
     if(this.output === '0'){
       if('0123456789'.indexOf(input) >=0 ){
         this.output = input
@@ -66,9 +66,9 @@ onOutputChange(){
       }
       return
     }
-    
+  
 
-    if(this.output.indexOf('.')>=0 && input === '.'){return}
+    if(this.output.indexOf('.')>=1 && input === '.'){return}
      this.output += input
   }
 
@@ -85,19 +85,16 @@ clear(){
  this.output = '0'
 }
 plus(){
-
-
-
+  
 }
+
+
+
 minus(){
 
 }
 ok(){
  
-}
-onInput(event: KeyboardEvent){
- const input = event.target as HTMLInputElement
- this.value = input.value
 }
 
 }
@@ -116,11 +113,16 @@ onInput(event: KeyboardEvent){
    height: 52%;
   .buttons{
     margin-top: 5px;
+    display: flex;
     @extend %clearFix;
     height: 100%;
-    > button{
-      width: 23%;
-      height: 15%;
+    .number{
+      width: 80%;
+    @extend %clearFix;
+
+      button{
+      width: 30%;
+      height: 17%;
       float: left;
       border: 2px solid black;
       background: white;
@@ -128,21 +130,40 @@ onInput(event: KeyboardEvent){
       margin: 1%;
       font-family:UD Digi Kyokasho NP-B;
       font-size: 20px;
-      &.ok{
-        height: 15%;
-        background:#F1C50C;
-      }
-      &.zero{
-        width: 23%;
-      }
-      img{
-         width: 30px;
-         height: 25px;
-         margin: 12px;
-      }
     }
+    }
+    
   }
 }
+
+.tool{
+    width: 20%;
+    @extend %clearFix;
+
+     button{
+      width: 100%;
+      height: 17%;
+      float: left;
+      border: 2px solid black;
+      background: white;
+      border-radius:10px ;
+      margin: 4% 4% 4% -6%;
+      font-family:UD Digi Kyokasho NP-B;
+      font-size: 20px;
+      &.day{
+        background: #F2A8A0;
+      }
+     &.ok{
+       height: 36%;
+       background: #F0C50C;
+     }
+     img{
+         width: 30px;
+         height: 15px;
+      }
+     }
+}
+
 .output-wrapper{
     border: 2px solid black;
     border-radius: 10px;
